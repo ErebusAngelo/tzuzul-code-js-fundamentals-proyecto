af2e895a9a12f5af0c8085afd7119edfa2fcb129
ErebusAngelo <erebus.dangelo@gmail.com>
2022-06-02 21:25:26 -0300

    bug vida negativa solucionado

--- combates/pelea.js
+++ combates/pelea.js
@@ -14,29 +14,35 @@ function TirarDados() {
 
 //se decide el ataque verdadero sumando el ataque + tirada de dados - defensa del oponente y se da el combate hasta que la vida de uno llegue a cero.
 export function pelea(statsPropias,statsEnemigo){
-       do{
+   
       
-         trueDamage = TirarDados()+statsPropias.ataque-statsEnemigo.defensa
+       
+       
+       
+       while(statsPropias.vida >0 && statsEnemigo.vida > 0){
            
-           enemyTrueDamage = TirarDados()+statsEnemigo.ataque-statsPropias.defensa
-          
-           if(trueDamage>enemyTrueDamage){
-            statsEnemigo.vida-=trueDamage
+
+           trueDamage = TirarDados()+statsPropias.ataque-statsEnemigo.defensa
+             
+             enemyTrueDamage = TirarDados()+statsEnemigo.ataque-statsPropias.defensa
+            
+             if(trueDamage>enemyTrueDamage){
+              statsEnemigo.vida-=trueDamage
+             }
+             else if(enemyTrueDamage>trueDamage ){
+                 statsPropias.vida-=enemyTrueDamage
+             }
+             else{
+                 alert ("Ataque esquivado")
+             }
+             turno++
+             alert("Turno "+(turno))
+             if (statsEnemigo.vida<0) {
+              statsEnemigo.vida=0
+              }
+             console.log(`Vida  ${statsPropias.vida}`)
+             console.log(`Vida del enemigo  ${statsEnemigo.vida}`)
            }
-           else if(enemyTrueDamage>trueDamage){
-               statsPropias.vida-=enemyTrueDamage
-           }
-           turno++
-           alert("Turno "+(turno))
-           if (statsEnemigo.vida<0) {
-            statsEnemigo.vida=0
-            }
-           console.log(`Vida  ${statsPropias.vida}`)
-           console.log(`Vida del enemigo  ${statsEnemigo.vida}`)
-        
-           }
-        
-           while(statsPropias.vida >0 && statsEnemigo.vida > 0)
            //se decide que pasa cuado la vida de alguno llega a cero
            if(statsPropias.vida<=0){
                alert("Haz muerto")
@@ -46,8 +52,9 @@ export function pelea(statsPropias,statsEnemigo){
                else{
                    alert("Haz derrotado a tu enemigo")
                   turno = 0
-               }
+                }
                
-        }
-       
-      
+            }
+    
+
+   
